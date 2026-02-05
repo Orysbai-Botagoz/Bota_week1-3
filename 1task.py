0fbc42904badfc7072cf4f0ce7db3950be0c4572
Orysbai-Botagoz <botagozorysbaj@gmail.com>
2026-02-05 10:29:15 +0500

    2-task

--- 1task.py
+++ 1task.py
@@ -23,3 +23,15 @@ def analyze_text(text):
     return (num_unique_vowels, " ".join(found_words))
 print (analyze_text("level radar apple banana civic kayak level stats world refer"))
 
+#2
+def words(text):
+    new_text = text.split()
+    filtered_text = list(filter(lambda x: not any(ch.isdigit() for ch in x), new_text))
+    reverse_text = list(map(lambda s: s[::-1], filtered_text))
+    result_text = ""
+    for word in reverse_text:
+        if len(word) % 2 == 0:
+            result_text += word + " "
+    return result_text.strip()
+print (words("hello abc123 world test44 noon even 2468 python code"))
+
