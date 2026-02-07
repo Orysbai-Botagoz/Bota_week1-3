334ee6a210a6fb10c0d98b75e149e2daf87a0792
Orysbai-Botagoz <botagozorysbaj@gmail.com>
2026-02-07 11:17:36 +0500

    3-task

--- lab-1.py
+++ lab-1.py
@@ -35,3 +35,29 @@ def words(text):
     return result_text.strip()
 print (words("hello abc123 world test44 noon even 2468 python code"))
 
+#3
+def top_k_words(text, k):
+    lower_text = text.lower()
+    clean_text = ""
+    for char in lower_text:
+        if "a" <= char <= "z" or char == " ":
+            clean_text += char
+    counts = {}
+    words = clean_text.split()
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    items = list(counts.items())
+    items.sort(key=lambda x: (-x[1], x[0]))
+
+    result = list()
+    for i in range(min(k, len(items))):
+        result.append(items[i][0])
+    return result
+
+
+print(top_k_words("Apple, banana! Apple orange banana apple. Orange banana?", 3))
+
